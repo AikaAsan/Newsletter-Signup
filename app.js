e15d4dcd3b3a16170bb511feba6756fa2396c610
aika <aiganysha@gmail.com>
2021-02-26 10:57:04 -0600

    ignore api key

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
+require('dotenv').config();
+
 
 
 const app = express();
@@ -12,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
-
+//POST request for home route
 app.post("/", function (req, res) {
     const firstName = req.body.fName;
     const lastName = req.body.lName;
@@ -33,13 +35,19 @@ app.post("/", function (req, res) {
     const jsonData = JSON.stringify(data);
 
     const url = "https://us1.api.mailchimp.com/3.0/lists/d7e1860791";
-
+    const api_key = process.env.API_KEY;
     const options = {
         method: "POST",
-        auth: "aika1:0752f909c4f66231e406250da16b7b3f-us1"
+        auth: "aika1:" + api_key
     }
 
     const request = https.request(url, options, function (response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
         response.on("data", function (data) {
             console.log(JSON.parse(data));
         })
@@ -50,14 +58,17 @@ app.post("/", function (req, res) {
 });
 
 
+app.post("/failure.html", function (req, res) {
+    res.redirect("/");
+});
 
-app.listen(3000, function () {
+
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running on port 3000.");
 });
 
 
-//API Key
-// 0752f909c4f66231e406250da16b7b3f-us1
+
 
 // List ID
 // d7e1860791
